65a793bb46c8ea4071f72d3abd26a38762b82b54
alemagno10 <alemagno03@gmail.com>
2022-12-20 15:41:12 -0300

    new features

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,7 +2,8 @@ class CalcController {
     constructor(){
         this.entrys = {'multiplicacao': '*', 'divisao': '/', 'soma': '+', 'subtracao': '-', 'porcento': '%', 'ponto': '.'};
         this._memory = [];
-        this.temp = 0;
+        this._temp = false;
+        this._resetMemory = false;
         this.locale = 'pt-BR';
         this._displayCalcEl = document.getElementById('display');
         this._dateEl = document.getElementById('data');
@@ -25,7 +26,7 @@ class CalcController {
     getLastIndex(i){return this._memory.length - i}
     getResult(){
         let result = eval(this._memory.join(' '));
-        if(result.toString().length > 10){result = result.toExponential(1)};
+        if(result.toString().length > 10){result = result.toFixed(8)};
         this._memory = [result.toString()]
     }
 
@@ -33,10 +34,8 @@ class CalcController {
         if((value === '.') && (isNaN(this.getLastValue(1) || this._memory.length === 0))){
             this._memory.push('0.')
         }
-
         else if(this._memory.length === 0 && !isNaN(value)){
             this._memory.push(value);
-            this.temp += 1
         }
         else if(!isNaN(this.getLastValue(1)) && (!isNaN(value) || value === '.')){
             this._memory[this.getLastIndex(1)] = this.getLastValue(1) + value; 
@@ -44,7 +43,15 @@ class CalcController {
         else if(isNaN(this.getLastValue(1)) && isNaN(value)){
             this._memory[this.getLastIndex(1)] = value
         }
-        else{this._memory.push(value)}  
+        else if(this._temp && isNaN(value)){
+            this.getResult()
+            this._memory.push(value);
+            this._temp = false;
+        }
+        else{
+            this._memory.push(value);
+            this._temp = true;
+        }  
     }
 
     execBtn(value){
@@ -59,10 +66,17 @@ class CalcController {
         else if(Object.keys(this.entrys).includes(value)){this.addEntry(this.entrys[value])}
 
         else if(value === 'igual'){
-            this.getResult()
+            this.getResult();
+            this._resetMemory = true;
         }
 
-        else{this.addEntry(value)};
+        else{
+            if(this._resetMemory === true){
+                this._memory = [];
+                this._resetMemory = false;
+            };
+            this.addEntry(value);
+        };
         this.displayCalc = this._memory.join(' ');
     }
 
